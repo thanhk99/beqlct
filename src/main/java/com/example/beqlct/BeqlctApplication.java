f27f1b1f50f53ce72a7332c539482e531b7238ae
thanhk99 <thanh040804@gmail.com>
2025-04-09 14:23:49 +0700

    first commit

---
+++ src/main/java/com/example/beqlct/BeqlctApplication.java
@@ -0,0 +1,13 @@
+package com.example.beqlct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeqlctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeqlctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beqlct/BeqlctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beqlct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeqlctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
